f432f2c34a8f3d3fe57d6343ad379e0af96b2e71
Rui Onodera <deraru@gmail.com>
2015-05-15 16:39:47 +0900

    Implement BonusDrink.bonus_count_for specs

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,20 +1,22 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    bonus = bonus_count_for(amount)
-    total = amount + bonus
-    total
-  end
+  class << self
+    def total_count_for(amount)
+      bonus = bonus_count_for(amount)
+      total = amount + bonus
+      total
+    end
 
-  private
+    private
 
-  def self.bonus_count_for(amount)
-    bonus = amount / 3
-    remain = amount % 3
-    rebonusable = bonus + remain
-    if rebonusable >= 3
-      bonus + bonus_count_for(rebonusable)
-    else
-      bonus
+    def bonus_count_for(amount)
+      bonus = amount / 3
+      remain = amount % 3
+      rebonusable = bonus + remain
+      if rebonusable >= 3
+        bonus + bonus_count_for(rebonusable)
+      else
+        bonus
+      end
     end
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,25 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  subject { BonusDrink.total_count_for(amount) }
-  context 'when buy 0' do
-    let(:amount) { 0 }
-    specify { expect(subject).to eq 0 }
+  describe 'BonusDrink.total_count_for' do
+    subject { BonusDrink.total_count_for(amount) }
+    context 'when buy 0' do
+      let(:amount) { 0 }
+      specify { expect(subject).to eq 0 }
+    end
+    context 'when buy 1' do
+      let(:amount) { 1 }
+      specify { expect(subject).to eq 1 }
+    end
+    context 'when buy 3' do
+      let(:amount) { 3 }
+      specify { expect(subject).to eq 4 }
+    end
+    context 'when buy 11' do
+      let(:amount) { 11 }
+      specify { expect(subject).to eq 16 }
+    end
+    context 'when buy 100' do
+      let(:amount) { 100 }
+      specify { expect(subject).to eq 149 }
+    end
   end
-  context 'when buy 1' do
-    let(:amount) { 1 }
-    specify { expect(subject).to eq 1 }
-  end
-  context 'when buy 3' do
-    let(:amount) { 3 }
-    specify { expect(subject).to eq 4 }
-  end
-  context 'when buy 11' do
-    let(:amount) { 11 }
-    specify { expect(subject).to eq 16 }
-  end
-  context 'when buy 100' do
-    let(:amount) { 100 }
-    specify { expect(subject).to eq 149 }
+  describe 'BonusDrink.bonus_count_for' do
+    subject { BonusDrink.send(:bonus_count_for, amount) }
+    context 'when buy 0' do
+      let(:amount) { 0 }
+      specify { expect(subject).to eq 0 }
+    end
+    context 'when buy 1' do
+      let(:amount) { 1 }
+      specify { expect(subject).to eq 0 }
+    end
+    context 'when buy 3' do
+      let(:amount) { 3 }
+      specify { expect(subject).to eq 1 }
+    end
+    context 'when buy 11' do
+      let(:amount) { 11 }
+      specify { expect(subject).to eq 5 }
+    end
+    context 'when buy 100' do
+      let(:amount) { 100 }
+      specify { expect(subject).to eq 49 }
+    end
   end
 end
