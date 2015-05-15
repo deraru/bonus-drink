75303a4a8484a20ce6afdf732ad1c505f3c44431
Rui Onodera <deraru@gmail.com>
2015-05-15 16:14:29 +0900

    Implement specs

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  subject { BonusDrink.total_count_for(amount) }
+  context 'when buy 0' do
+    let(:amount) { 0 }
+    specify { expect(subject).to eq 0 }
+  end
+  context 'when buy 1' do
+    let(:amount) { 1 }
+    specify { expect(subject).to eq 1 }
+  end
+  context 'when buy 3' do
+    let(:amount) { 3 }
+    specify { expect(subject).to eq 4 }
+  end
+  context 'when buy 11' do
+    let(:amount) { 11 }
+    specify { expect(subject).to eq 16 }
+  end
 end
